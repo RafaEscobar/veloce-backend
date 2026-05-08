2d975b0f58eef2c06ee720101b4a74de6a45d2fe
Rafa Escobar <Rafael.EG.code@gmail.com>
2026-05-07 20:50:08 -0600

    Agrega ruta de vehicles bajo middleware

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\VehicleController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('vehicles', VehicleController::class);
+});
