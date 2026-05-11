e702c0a5b21dd25cdb080b8f7f4e781c5dee6276
Rafa Escobar <Rafael.EG.code@gmail.com>
2026-05-11 14:43:19 -0600

    Agrega API para listar tipos de vehiculos

--- app/Http/Controllers/Api/VehicleTypeController.php
+++ app/Http/Controllers/Api/VehicleTypeController.php
@@ -5,7 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Http\Resources\VehicleTypeCollection;
+use App\Models\VehicleType;
+
 class VehicleTypeController extends Controller
 {
-    //
+    public function index(): VehicleTypeCollection
+    {
+        $vehicleTypes = VehicleType::paginate();
+
+        return new VehicleTypeCollection($vehicleTypes);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\VehicleController;
+use App\Http\Controllers\Api\VehicleTypeController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register'])
@@ -12,4 +13,5 @@ Route::post('/login', [AuthController::class, 'login'])
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('vehicles', VehicleController::class);
+    Route::get('vehicle-types', [VehicleTypeController::class, 'index']);
 });
